80eb867a12b44658a50ad184a6bcad669d2671dd
DogSniffers <DogSniffers@users.noreply.github.com>
2020-01-24 16:20:06 -0700

    yurhur upload 2

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -19,7 +19,7 @@ const name = "Noah Van Buskirk"
 //////////////////STEP 4////////////////////
 //Create a variable called 'phrase' that is initialized as an empty string. 
 
-const phrase = ``
+var phrase 
 
 //////////////////STEP 5////////////////////
 //Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
@@ -27,10 +27,10 @@ const phrase = ``
 
     function code(){
         if(lovesCode === true){
-        phrase = 'I love code'
+         phrase = `I love code`
        return
 }else {
-     return  phrase = 'I do not love to code'
+     return  phrase = `I do not love to code`
 }}
 code()
 
